54591367325d29f385488a5a94d73b1dc0cc8344
luckys-lnz <lalucky22@gmail.com>
2024-10-14 11:50:58 +0000

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""0. Basic annotations - add module
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Type-annonated python function that takes
+    a floats a and b and returns the sum as a float.
+    """
+    return a + b
