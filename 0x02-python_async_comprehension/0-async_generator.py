15675e7a9ae8e4c11e8aa13da712b9319a9b0741
luckys-lnz <lalucky22@gmail.com>
2024-10-15 21:00:10 +0000

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generate random numbers.
 
@@ -18,8 +19,7 @@ async def async_generator():
         float: A random float between 0 and 10.
     """
 
-    i = 0
-    while i < 10:
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        i += 1
+
